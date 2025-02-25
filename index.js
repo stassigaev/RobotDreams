10bde8403f36af974186fd660e773f6e8249fc7a
stassigaev <stassigaev@gmail.com>
2025-02-25 23:32:21 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function sayHello(name, surname, someOtherWorld2) {
+  console.log(`Hello ${name} ${surname} ${someOtherWorld2}`);
+  // Some comment
+}
+
+sayHello('Stan', 'Hill');
+
+// end function
