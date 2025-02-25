8ddb9b4511c2a9d6ca90913db281c37d0a907388
stassigaev <stassigaev@gmail.com>
2025-02-25 23:49:07 +0200

    Test

--- index.js
+++
@@ -1,8 +0,0 @@
-function sayHello(name, surname, someOtherWorld2) {
-  console.log(`Hello ${name} ${surname} ${someOtherWorld2}`);
-  // Some comment
-}
-
-sayHello('Stan', 'Hill');
-
-// end function
